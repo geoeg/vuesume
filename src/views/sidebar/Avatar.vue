81226ffcb97b449730c4353ef939e0dfd60b963b
Georgi Kolev <georgi.lyubomirov.kolev@gmail.com>
2020-03-13 18:20:03 +0200

    fix avatar responsiveness

--- src/views/sidebar/Avatar.vue
+++ src/views/sidebar/Avatar.vue
@@ -1,18 +1,25 @@
 <template>
-    <v-avatar class="avatar">
-      <img class="avatar" src="../../../public/geoeg-avatar.jpg" alt="Georgi Kolev" />
-    </v-avatar>
+  <v-avatar
+    class="avatar"
+    size="100%"
+    max-height="200"
+    max-width="200"
+    min-height="50"
+    min-width="50"
+  >
+    <img src="../../../public/geoeg-avatar.jpg" alt="Georgi Kolev" />
+  </v-avatar>
 </template>
 
 <script>
 export default {
   // name: "Avatar"
-}
+};
 </script>
 
 <style scoped>
-  .avatar {
+/* .avatar {
     width: 200px !important;
     height: 200px !important;
-  }
+  } */
 </style>
